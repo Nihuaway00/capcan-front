7431a3a7794baaf1fb6be22e773ada6b9cd05976
Nihuaway00 <nikitaemaeny@gmail.com>
2024-08-18 13:07:23 +0300

    fix padding in seller page

--- src/features/SellerForm.jsx
+++ src/features/SellerForm.jsx
@@ -2,13 +2,13 @@ import {Button, Stack} from "react-bootstrap";
 
 export function SellerForm() {
     return (
-        <Stack>
+        <Stack gap={2}>
             <Button>Скопировать карточки в ДРУГОЙ магазин</Button>
             <Button>Скопировать карточки в ЭТОТ магазин</Button>
             <Button>Увеличить кол-во карточек в магазине</Button>
             <Button>Изменить экономику товаров</Button>
 
-            <Button className="mt-2">Изменить API ключ селлера</Button>
+            <Button className="mt-4">Изменить API ключ селлера</Button>
         </Stack>
     )
 }
