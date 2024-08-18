da74149b74441642559df61014acb97a20d596bc
Nihuaway00 <nikitaemaeny@gmail.com>
2024-08-18 12:32:51 +0300

    fix padding AddSellerPage

--- src/features/AddSellerForm.jsx
+++ src/features/AddSellerForm.jsx
@@ -8,25 +8,28 @@ export function AddSellerForm() {
     const [sellerApi, setSellerApi] = useState(null);
 
     return (
-        <Stack>
-            <InputField
-                value={sellerApi}
-                onChange={(e) => setSellerApi(e.target.value)}
-                label="API ключ селлера которого надо добавить*"
-                placeholder="API - ключ селлера"
-            />
-
+        <Stack gap={5}>
             <Stack>
-                <h5>Юнит экономика товаров*</h5>
-                <p>
-                    Чтобы добавление селлера было корректно, добавьте юнитку, согласно примеру. Проверьте Свою юнитку,
-                    перед добавлением
-                </p>
-                <IconButton icon={<CardChecklist size={24}/>} variant={"outline-secondary"}>
-                    Загрузить юнит экономику товаров
-                </IconButton>
+                <InputField
+                    value={sellerApi}
+                    onChange={(e) => setSellerApi(e.target.value)}
+                    label="API ключ селлера которого надо добавить*"
+                    placeholder="API - ключ селлера"
+                />
+
+                <Stack>
+                    <h5>Юнит экономика товаров*</h5>
+                    <p>
+                        Чтобы добавление селлера было корректно, добавьте юнитку, согласно примеру. Проверьте Свою юнитку,
+                        перед добавлением
+                    </p>
+                    <IconButton icon={<CardChecklist size={24}/>} variant={"outline-secondary"}>
+                        Загрузить юнит экономику товаров
+                    </IconButton>
+                </Stack>
             </Stack>
-            <Button>
+
+            <Button href="/set">
                 Готово
             </Button>
         </Stack>
