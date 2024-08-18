f56d738e5c6177c19bb8c71c53a26874992af0ef
Nihuaway00 <nikitaemaeny@gmail.com>
2024-08-18 13:25:44 +0300

    add fake seller data loading

--- src/pages/SellerPage.jsx
+++ src/pages/SellerPage.jsx
@@ -1,10 +1,42 @@
 import {Stack} from "react-bootstrap";
 import {PageTop} from "../components/PageTop";
 import {SellerForm} from "../features/SellerForm";
+import {useLocation, useParams} from "react-router-dom";
+import {useMutation, useQuery} from "react-query";
+import axios from "axios";
+
+function useSeller(id){
+    function getSeller(){
+        if(!id) return null;
+        const data = {
+            name: "Хуй сунь вынь & " + id
+        }
+        //const {data} = axios.get(`/api/seller/${id}`);
+        return data;
+    }
+
+    return useQuery("seller", getSeller)
+}
 
 export function SellerPage() {
+    const {id} = useParams();
+    const {data, isLoading, isSuccess, error} = useSeller(id);
+
+    if(isLoading){
+        return "Загрука...";
+    }
+
+    if(error){
+        return error.message;
+    }
+
+
+    if(!data){
+        return "data is null:" + data;
+    }
+
     const pageInfo = {
-        title: "IIIIIIIIIIIIII", //TODO: откуда мне брать имя селлера?
+        title: data.name, //TODO: откуда мне брать имя селлера?
         backLink: "/set"
     }
 
