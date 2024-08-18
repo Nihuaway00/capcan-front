6bc6aba7a4600332d42f3b67c260e6dd43ab628d
Nihuaway00 <nikitaemaeny@gmail.com>
2024-08-18 13:05:52 +0300

    move functionality code from page to form

--- src/features/CopyForm.jsx
+++ src/features/CopyForm.jsx
@@ -1,13 +1,39 @@
 import {InputField} from "../components/InputField";
-import {Button, Col, Form, Modal, Row} from "react-bootstrap";
+import {Button, Col, Form, ListGroup, Modal, Row, Stack} from "react-bootstrap";
 import React, {useEffect, useState} from "react";
 import {MarginSettings} from "../components/MarginSettings";
 import {CheckBox} from "../components/CheckBox";
 import {useMutation} from "react-query";
 import {Alert} from "../components/Alert";
-import {CardChecklist, CheckLg, Clipboard2Heart} from "react-bootstrap-icons";
+import {CardChecklist, CheckLg, Clipboard2Heart, Plus} from "react-bootstrap-icons";
 import {SuccessCopyAlert} from "../components/prefabs/SuccessCopyAlert";
 import {BadCopyAlert} from "../components/prefabs/BadCopyAlert";
+import {IconButton} from "../components/IconButton";
+
+function SellerList(){
+    const [sellers, setSellers] = useState([
+        {id: 1434,name: "Seller 1", api: "fkewjfpwegmewpfg343"},
+        {id: 3434,name: "Seller 2", api: "fewfm34ifj344plhptl"},
+    ]);
+
+    return (
+        <Stack className="mb-4">
+            <Stack direction="horizontal" className="flex justify-content-between mb-4 align-items-center">
+                <h2 className="mb-0">Селлеры</h2>
+                <IconButton href={"/set/add-seller"} icon={<Plus size={22} />}>
+                    Добавить селлера
+                </IconButton>
+            </Stack>
+            <ListGroup>
+                {
+                    sellers.map((seller, index) => (
+                        <ListGroup.Item action href={"/set/seller/" + seller.id} key={index}>{seller.name}</ListGroup.Item>
+                    ))
+                }
+            </ListGroup>
+        </Stack>
+    )
+}
 
 export function CopyForm() {
     const [fromApi, setFromApi] = useState("");
@@ -37,7 +63,7 @@ export function CopyForm() {
 
 
     return (
-        <>
+        <Stack>
             {
                 copyMutation.isSuccess || <SuccessCopyAlert
                     show={showAlert}
@@ -77,6 +103,7 @@ export function CopyForm() {
             <Row className="mb-4 px-2 w-auto d-flex justify-content-end">
                 <Button onClick={copyHandler} type="button">Начать копирование</Button>
             </Row>
-        </>
+            <SellerList />
+        </Stack>
     )
 }
--- src/pages/StartCopyPage.jsx
+++ src/pages/StartCopyPage.jsx
@@ -19,19 +19,7 @@ export function StartCopyPage() {
                 </Stack>
             </Stack>
             <CopyForm />
-            <Stack className="mb-4">
-                <Stack direction="horizontal" className="flex justify-content-between mb-4 align-items-center">
-                    <h2 className="mb-0">Селлеры</h2>
-                    <IconButton href={"/set/add-seller"} icon={<Plus size={26} />}>
-                        Добавить селлера
-                    </IconButton>
-                </Stack>
-                <ListGroup>
-                    <ListGroup.Item>Seller 1</ListGroup.Item>
-                    <ListGroup.Item>Seller 2</ListGroup.Item>
-                    <ListGroup.Item>Seller 3</ListGroup.Item>
-                </ListGroup>
-            </Stack>
+
         </Stack>
     );
 }
